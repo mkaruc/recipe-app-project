4f10a310afaad6554720d510a57cb5c9ea784a1e
mkaruc <fatihmehmetkaruc@gmail.com>
2022-07-16 13:36:26 +0300

    classnotes

--- rc16/context-example2/src/App.js
+++ rc16/context-example2/src/App.js
@@ -4,15 +4,11 @@ import ShowUsers from './pages/ShowUsers';
 function App() {
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    fetch('https://api.github.com/users')
-      .then((res) => res.json())
-      .then((data) => setUsers(data));
-  }, []);
+  
 
   return (
     <>
-      <ShowUsers users={users} />
+      <ShowUsers />
     </>
   );
 }
